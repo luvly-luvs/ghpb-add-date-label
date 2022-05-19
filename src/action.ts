c7ad630aa083f4077e5f1125238b5ad268b72007
aschen-builder <auschick.work@gmail.com>
2022-05-18 23:53:31 -0700

    update query vars

--- src/action.ts
+++ src/action.ts
@@ -26,7 +26,7 @@ const action = async (args: ActionArgs) => {
     const { graphql } = getAuthenticatedOctokit(authArgs);
 
     const projectFields = await graphql<ProjectFieldsQueryResult>({
-      query: `query {
+      query: `query getProjectFields($projectId: String!) {
         node(id: $projectId) {
           ... on ProjectNext {
             fields(first: 10) {
